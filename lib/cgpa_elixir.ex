3034ecee975d19a698b86da6681a0ee33dd3d5e1
steph-crown <emmanuelstephen024@gmail.com>
2022-05-19 12:25:34 +0100

    Create new elixir project with * creating README.md * creating .formatter.exs * creating .gitignore * creating mix.exs * creating lib * creating lib/cgpa_elixir.ex * creating test * creating test/test_helper.exs * creating test/cgpa_elixir_test.exs
    Your Mix project was created successfully.
    You can use "mix" to compile it, test it, and more:
    
        cd ./cgpa_elixir
        mix test
    
    Run "mix help" for more commands.

---
+++ lib/cgpa_elixir.ex
@@ -0,0 +1,18 @@
+defmodule CgpaElixir do
+  @moduledoc """
+  Documentation for `CgpaElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CgpaElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cgpa_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule CgpaElixirTest do
+  use ExUnit.Case
+  doctest CgpaElixir
+
+  test "greets the world" do
+    assert CgpaElixir.hello() == :world
+  end
+end
